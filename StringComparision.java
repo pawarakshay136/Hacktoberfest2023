c2c3ae02c9b44e8a86be3ac202a2045c3f3238b1
pawarakshay136 <140945443+pawarakshay136@users.noreply.github.com>
2023-10-27 15:32:59 +0530

    Create StringComparision.java

---
+++ StringComparision.java
@@ -0,0 +1,14 @@
+public class StringComparision {
+
+	public static void main(String[] args) {
+		
+		String Launguage= "C#";
+		
+		if (Launguage=="Java") {
+			System.out.println("Its Easy Launguage");
+		}else {
+			System.out.println("Its Difficult Launguage");
+		}
+	}
+	
+}
